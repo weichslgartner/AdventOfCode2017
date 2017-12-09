6a6dda3f06e63289f4751f59469dc473dced7d53
weichslgartner <andreas.weichslgartner@fau.de>
2017-12-09 15:44:00 +0100

    solution of Day 9

---
+++ src/day9/day9.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"util"
+	"fmt"
+)
+
+func getScore(input string) (int,int) {
+	score :=0
+	isGarbage := false
+	groupsOpen := 0
+	canceledCharacters :=0
+	currentCharPos := 0
+	for currentCharPos < len(input){
+		currentChar := string(input[currentCharPos])
+		//println(currentChar)
+
+		switch currentChar {
+		case "!":
+			currentCharPos+=2
+		case "<":
+			if isGarbage{
+				canceledCharacters++
+			}
+			isGarbage = true
+			currentCharPos++
+		case ">":
+			isGarbage = false
+			currentCharPos++
+		case "{":
+			if !isGarbage{
+				groupsOpen++
+			}else{
+				canceledCharacters++
+			}
+			currentCharPos++
+		case "}":
+			if !isGarbage{
+				score += groupsOpen
+				groupsOpen--
+			}else{
+				canceledCharacters++
+			}
+			currentCharPos++
+		default:
+			if isGarbage{
+				canceledCharacters++
+			}
+			currentCharPos++
+		}
+	}
+	return score, canceledCharacters
+}
+
+
+func main() {
+	input := util.ReadFileToString("inputs/day9.txt")
+	score, canceledCharacters := getScore(input)
+	fmt.Println("Score: ",score)
+	fmt.Println("Canceled characters: ",canceledCharacters)
+
+}
+
