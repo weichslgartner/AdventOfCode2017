6b90a76f23a91205339c04d8e10ce1f7a52a562c
weichslgartner <andreas.weichslgartner@fau.de>
2017-12-14 22:25:22 +0100

    solution of Day 14

---
+++ src/day14/day14.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"util"
+	"fmt"
+	"strconv"
+)
+
+const FREE = 0
+const OCCUPIED = -1
+
+var diskArray [][]int
+
+func toOccupiedFree(array []int) []int {
+	result := make([]int, len(array))
+	for i, cell := range array {
+		if cell == 1 {
+			result[i] = OCCUPIED
+		} else {
+			result[i] = FREE
+		}
+
+	}
+	return result
+}
+
+func printGrid() {
+	for _, line := range diskArray {
+		for _, char := range line {
+
+			if char == OCCUPIED {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+
+		}
+		fmt.Println()
+	}
+}
+
+func printArray() {
+	for _, line := range diskArray {
+		for _, char := range line {
+
+			fmt.Printf("|%04d", char)
+		}
+		fmt.Println()
+	}
+}
+
+func findRegions() int {
+	numberRegions := 1
+	for i, line := range diskArray {
+
+		for j, char := range line {
+			if char == OCCUPIED {
+				searchRegion(i, j, numberRegions)
+				numberRegions++
+			}
+
+		}
+
+	}
+	//fmt.Println(numberRegions-1)
+	return numberRegions - 1
+}
+
+func searchRegion(y int, x int, regionNumber int) {
+	diskArray[y][x] = regionNumber
+	if y > 0 && diskArray [y-1][x] == OCCUPIED {
+		searchRegion(y-1, x, regionNumber)
+	}
+	if y < 128-1 && diskArray [y+1][x] == OCCUPIED {
+		searchRegion(y+1, x, regionNumber)
+	}
+	if x > 0 && diskArray [y][x-1] == OCCUPIED {
+		searchRegion(y, x-1, regionNumber)
+	}
+	if x < 128-1 && diskArray [y][x+1] == OCCUPIED {
+		searchRegion(y, x+1, regionNumber)
+	}
+
+	return
+}
+
+func main() {
+	numberOnes := 0
+	input := "ljoxqyyw" //"flqrgnkx"//
+	diskArray = make([][]int, 128)
+
+	for i := 0; i < 128; i++ {
+		hashInput := input + "-" + strconv.Itoa(i)
+		hash := util.KnotHash(hashInput)
+		binHash := util.StringToBin(hash)
+		diskArray[i] = toOccupiedFree(util.StringToIntArray(binHash))
+		numberOnes += util.HammingWeight(binHash)
+	}
+	//printGrid()
+	//printArray()
+	fmt.Println("Part 1:", numberOnes)
+	fmt.Println("Part 2:", findRegions())
+
+}
--- src/util/util.go
+++ src/util/util.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"strconv"
 	"regexp"
+	"fmt"
 )
 
 
@@ -79,3 +80,119 @@ func IntListToString(intSlice []int) string{
 
 }
 
+func mod(number int,length int)int{
+	if number < 0 {
+		return mod(length +number, length)
+	}else{
+		return number % length
+	}
+}
+
+func revertSubList(list []int, start int, stop int,length int){
+	copyList := make([]int,len(list))
+	copy(copyList,list)
+	for i:=0; i < length;i++{
+		index1:= mod(start+i,len(list))
+		index2:= mod(stop-i,len(list))
+		list[index1] = copyList[index2]
+	}
+}
+
+func doTheHash(list []int, inputLengths []int, numberRounds int){
+	start := 0
+	skipSize :=0
+	for i:=0; i < numberRounds; i++{
+		for _,length :=range inputLengths{
+			startInvert := (start) % len(list)
+			endInvert := (start+length-1) % len(list)
+			revertSubList(list,startInvert,endInvert,length)
+			start += length+skipSize
+			skipSize++
+		}
+	}
+
+}
+
+
+func convertToDenseHash(list []int) []int{
+	denseHash := make([]int,len(list)/16)
+	for i:=0; i < len(list);i+=16{
+		for j:=0; j <16; j++{
+			denseHash[i/16] ^= list[i+j]
+		}
+	}
+	return denseHash
+}
+
+func convertToHex(list []int) string{
+	hexString :=""
+	for _,element := range list{
+		hexString += fmt.Sprintf("%.02x", element)
+	}
+	return hexString
+}
+func generateList(length int) []int{
+	intList := make([]int, length)
+	for i :=0; i < length; i++{
+		intList[i] = i
+	}
+	return intList
+}
+
+func getHashInput(input string) []int {
+	byteInput := []byte(input)
+	newInput := append(byteInput, 17, 31, 73, 47, 23)
+	intLengths := make([]int,len(newInput))
+	for i,element := range newInput{
+		intLengths[i] = int(element)
+	}
+	return intLengths
+}
+
+func KnotHash(input string) string{
+	hashList := generateList(256)
+	doTheHash(hashList, getHashInput(input), 64)
+	denseHash := convertToDenseHash(hashList)
+	return convertToHex(denseHash)
+}
+
+
+func StringToBin(input string)  string {
+	binaryString:=""
+	for _, c := range input {
+		intHex ,_ :=  strconv.ParseUint(string(c), 16, 32)
+		binaryString += fmt.Sprintf("%04b",intHex)
+
+	}
+	return binaryString
+}
+
+func HammingWeight(input string) int{
+	numberOnes := 0
+	for _,bit := range input{
+		if string(bit) =="1"{
+			numberOnes++
+		}
+	}
+	return numberOnes
+}
+
+func BinToGrid(input string)  string{
+	result := ""
+	for _,bit := range input {
+		if string(bit) == "1"{
+			result += "#"
+		}else{
+			result += "."
+		}
+	}
+	return result
+}
+
+func StringToIntArray(input string) []int{
+	result := make([]int,len(input))
+	for i,char := range input{
+		result[i],_ = strconv.Atoi(string(char))
+	}
+	return result
+}
